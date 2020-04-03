90751702903165864eee76fb4b8932c41cee5896
Loïc Feuga <loic.feuga@capgemini.com>
2020-04-03 21:22:01 +0200

    Init solver with mocked input

--- src/main/java/current_exercise/Main.java
+++ src/main/java/current_exercise/Main.java
@@ -1,16 +1,17 @@
 package current_exercise;
 
 import exercise1.Solver;
+
+import java.io.IOException;
 //import exercise2.Solver;
 //import exercise3.Solver;
 //import exercise4.Solver;
 //import exercise5.Solver;
 
 public class Main {
-    private static int currentExercise = 1;
 
-    public static void main(String[] args) {
-        Solver solver = new Solver(args);
+    public static void main(String[] args) throws IOException {
+        Solver solver = new Solver(args, true);
 
     }
 
